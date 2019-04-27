80475e6348202b2432d12d447353b6a602becd5d
Cake42 <heitortoledo@hotmail.com>
2019-04-27 16:03:11 -0300

    Added Bubble and Quick sorts in C#.

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,53 @@
+﻿namespace H004
+{
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+
+    public static class BubbleSort
+    {
+        public static void Main()
+        {
+            var rng = new Random();
+            const int Size = 99999;
+            int[] array = Enumerable
+                .Range(0, Size)
+                .OrderBy(_ => rng.Next())
+                .ToArray();
+
+            var sw = new Stopwatch();
+            sw.Start();
+            Sort(array, Size);
+            sw.Stop();
+
+            Console.WriteLine($"Bubble Sort: {sw.ElapsedMilliseconds}ms.");
+            Console.ReadKey();
+        }
+
+        public static void Sort(int[] vetor, int tamanho)
+        {
+            int j = 0;
+            int trab;
+            bool troca = true;
+            int limite = tamanho - 1;
+
+            while (troca)
+            {
+                troca = false;
+                for (int i = 0; i < limite; ++i)
+                {
+                    if (vetor[i] > vetor[i + 1])
+                    {
+                        trab = vetor[i];
+                        vetor[i] = vetor[i + 1];
+                        vetor[i + 1] = trab;
+                        j = i;
+                        troca = true;
+                    }
+                }
+
+                limite = j;
+            }
+        }
+    }
+}
---
+++ QuickSort/QuickSort.cs
@@ -0,0 +1,68 @@
+﻿namespace H004
+{
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+
+    public static class QuickSort
+    {
+        public static void Main()
+        {
+            var rng = new Random();
+            const int Size = 99999;
+            int[] array = Enumerable
+                .Range(0, Size)
+                .OrderBy(_ => rng.Next())
+                .ToArray();
+
+            var sw = new Stopwatch();
+            sw.Start();
+            Sort(array, Size, 0, Size - 1);
+            sw.Stop();
+
+            Console.WriteLine($"Quick Sort: {sw.ElapsedMilliseconds}ms.");
+            Console.ReadKey();
+        }
+
+        public static void Sort(int[] vetor, int tamanho, int i, int j)
+        {
+            int trab, esq, dir, pivo;
+            esq = i;
+            dir = j;
+
+            pivo = vetor[(int)Math.Round((esq + dir) / 2.0)];
+            do
+            {
+                while (vetor[esq] < pivo)
+                {
+                    esq++;
+                }
+
+                while (vetor[dir] > pivo)
+                {
+                    dir--;
+                }
+
+                if (esq <= dir)
+                {
+                    trab = vetor[esq];
+                    vetor[esq] = vetor[dir];
+                    vetor[dir] = trab;
+                    esq++;
+                    dir--;
+                }
+            }
+            while (esq <= dir);
+
+            if (dir - i >= 0)
+            {
+                Sort(vetor, tamanho, i, dir);
+            }
+
+            if (j - esq >= 0)
+            {
+                Sort(vetor, tamanho, esq, j);
+            }
+        }
+    }
+}
